5df2f413ec241aeae1c7188f7dbdc071fa0287ad
Yong Seon Shin <asdf4503@gwnu.ac.kr>
2023-02-02 19:11:54 +0900

    commit

--- src/M1065.java
+++ src/M1065.java
@@ -1,3 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//백준 한수구하기
 public class M1065 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = 0;
+
+        //정수
+        n = Integer.parseInt(bf.readLine());
+        
+        //등차수열 만들기
+
+        
+    }
+
 
 }
--- src/M1236.java
+++ src/M1236.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+//백준 성지키기
 public class M1236 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
