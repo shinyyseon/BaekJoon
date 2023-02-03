abfc9b23df066018a7b5291ae150e96522d7d2a6
Yong Seon Shin <asdf4503@gwnu.ac.kr>
2023-02-03 23:27:35 +0900

    commit

--- src/M1065.java
+++ src/M1065.java
@@ -7,14 +7,43 @@ public class M1065 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int n = 0;
+        int sum = 0;
+        int i = 1;
+        int count = 0;
 
-        //정수
+        //정수 입력
         n = Integer.parseInt(bf.readLine());
         
-        //등차수열 만들기
+        //정수가 1일 때
+        if(n == 1)      count = 1;
 
-        
+        //정수가 1보다 클 때
+        if(n > 1) {
+            while(true) {
+                sum = sum + i;
+                if(sum == n) {
+                    count++;
+                    i++;
+                    sum = 0;
+                }
+                else if(sum > n){
+                    i++;
+                    sum = 0;
+                    continue;
+                }
+                else {
+                    i++;
+                    sum = 0;
+                    continue;
+                }
+
+                if(i > n) {
+                    break;
+                }
+            }
+        }
+
+        //한수 출력
+        System.out.print(count);
     }
-
-
 }
