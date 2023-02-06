0b15bbea006131306eb8b607e58587cbc6991a46
Yong Seon Shin <asdf4503@gwnu.ac.kr>
2023-02-06 23:36:25 +0900

    commit

--- src/M2441.java
+++ src/M2441.java
@@ -6,8 +6,10 @@ public class M2441 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
+        //입력
         int n = Integer.parseInt(bf.readLine());
 
+        //출력
         for(int i = 0;i < n;i++){
             for(int j = 0;j < i;j++)
                 System.out.print(" ");
