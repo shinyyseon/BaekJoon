69db5b999bb4f46209270c2f216526d2ed3fc24c
Yong Seon Shin <asdf4503@gwnu.ac.kr>
2023-02-08 19:25:09 +0900

    commit

---
+++ src/M1010.java
@@ -0,0 +1,5 @@
+public class M1010 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/M1016.java
+++ src/M1016.java
@@ -2,35 +2,47 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class M1016 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
 
-        long count = 0, result = 0, total = 0;
-        long check = 0;
+        long check = 0, count = 0;
         long min = Long.parseLong(st.nextToken());
         long max = Long.parseLong(st.nextToken());
-        long[] prime = new long[(int) (min + 1000000)];
+        boolean[] num = new boolean[Math.toIntExact((max + 1))];
+        Arrays.fill(num, false);
 
-        //소수 구하기
-        for(long i = 2, index = 0;i<=max;i++) {
-            for(long j = 2;j<=i;j++)
-                if(i%j == 0)      check ++;
-            if(check == 1)        prime[(int) index] = i;
-            check = 0;
-            index++;
-        }
-
-        for( ;min <= max;min++){
-            for(long i : prime) {
-                result = min % prime[(int) i];
-                if (result == 0) count++;
-                total++;
+        //제곱ㄴㄴ 수 구하기
+        if(min >= 1 && max >= min) {
+            for(long i = 2;i <= max;i++) {
+                for(long j = 2;j <= i;j++)
+                    if(i % j == 0)      check ++;
+                if(check == 1) {
+                    for(long temp = min;temp <= max;temp++) {
+                        if(temp % (i * i) == 0) {
+                            if(!(num[(int) temp - 1])) {
+                                num[(int) temp - 1] = true;
+                            }
+                        }
+                    }
+                }
+                check = 0;
             }
-        }
 
-        System.out.print(total - count);
+            for( ;min <= max;min++) {
+                if(min == max) {
+                    if(!(num[(int) min - 1])){
+                        count++;
+                    }
+                }
+                else {
+                    if(!(num[(int) min])) count++;
+                }
+            }
+            System.out.print(count);
+        }
     }
 }
