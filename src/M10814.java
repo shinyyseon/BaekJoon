08136b5f33aad23b00e7b86f5c8de6aed28289d9
Yong Seon Shin <asdf4503@gwnu.ac.kr>
2023-03-03 17:34:01 +0900

    commit

--- src/M10814.java
+++ src/M10814.java
@@ -1,7 +1,32 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
 
 public class M10814 {
     public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
+        int n = Integer.parseInt(bf.readLine());
+        String[][] array = new String[n][2];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(bf.readLine());
+            array[i][0] = st.nextToken();
+            array[i][1] = st.nextToken();
+        }
+
+        Arrays.sort(array, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] s1, String[] s2) {
+                return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
+            }
+        });
+
+        for(int i = 0;i < n;i++)
+            System.out.println(array[i][0] +" "+ array[i][1]);
     }
 }
