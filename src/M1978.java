7837eb7eecb64b3ac0b24c0be8ca00dfce8171b3
Yong Seon Shin <asdf4503@gwnu.ac.kr>
2023-03-08 17:59:35 +0900

    commit

---
+++ src/M1978.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class M1978 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int n = Integer.parseInt(bf.readLine());
+        String[] array = new String[n+1];
+        Arrays.fill(array, "0");
+        array[0] = "0"; array[1] = "0";
+
+        for(int i = 0;i < n;i++) {
+            array[i] = Arrays.toString(bf.readLine().split(" "));
+        }
+
+        for(int i = 2;i * i < array.length;i++) {
+            for(int j = i + i;j < array.length;j += i) {
+                array[j] = "0";
+            }
+        }
+    }
+}
--- src/M2609.java
+++ src/M2609.java
@@ -1,12 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class M2609 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
 
-        String[] num = bf.readLine().split(" ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
+        int result = 1;
+
+        for(int i = 2;i <= n || i <= m;i++) {
+            if(n % i == 0 && m % i == 0)     result = i;
+        }
+
+        System.out.println(result);
+        System.out.println(n * m / result);
     }
 }
