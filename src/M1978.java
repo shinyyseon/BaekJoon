0f58d4ad594f885081765bd43c892cd8b5c1761f
Yong Seon Shin <asdf4503@gwnu.ac.kr>
2023-03-12 15:58:31 +0900

    commit

--- src/M1978.java
+++ src/M1978.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class M1978 {
     public static void main(String[] args) throws IOException {
@@ -9,17 +8,29 @@ public class M1978 {
 
         int n = Integer.parseInt(bf.readLine());
         String[] array = bf.readLine().split(" ");
+        int result = 0;
 
-        for(int i = 2;i < n;i++) {
+        for(int i = 0;i < n;i++) {
             int count = 0;
-            for(int j = 2;j < i;j++) {
-                if(i % j == 0)  count++;
+            if(Integer.parseInt(array[i]) == 1 || Integer.parseInt(array[i]) == 0) {
+                array[i] = "0";
+                continue;
             }
-            if(count > 1)   array[i] = "0";
+            for(int j = 2;j <= Integer.parseInt(array[i]);j++) {
+                if(Integer.parseInt(array[i]) % j == 0)
+                    count++;
+            }
+            if(count > 1)   {
+                array[i] = "0";
+            }
         }
 
         for(int i = 0;i < array.length;i++) {
-            System.out.println(array[i]);
+            if(Integer.parseInt(array[i]) != 0) {
+                result++;
+            }
         }
+
+        System.out.print(result);
      }
 }
