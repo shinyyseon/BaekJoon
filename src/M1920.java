093d7d531d01f9c93973ca904f046a8f35327bf0
Yong Seon Shin <asdf4503@gwnu.ac.kr>
2023-03-14 14:59:29 +0900

    commit

--- src/M1920.java
+++ src/M1920.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class M1920 {
     public static void main(String[] args) throws IOException {
@@ -9,9 +10,11 @@ public class M1920 {
 
         int N = Integer.parseInt(bf.readLine());
         String[] A = bf.readLine().split(" ");
+        Arrays.sort(A);
 
         int M = Integer.parseInt(bf.readLine());
         String[] array = bf.readLine().split(" ");
+        Arrays.sort(array);
 
         for(int i = 0;i < N;i++) {
             int count = 0;
@@ -20,9 +23,14 @@ public class M1920 {
                     sb.append("1").append("\n");
                     break;
                 }
-                else count++;
+                else if(Integer.parseInt(A[i]) > Integer.parseInt(array[j])) {
+                    count++;
+                    break;
+                }
+                if(count > 1) {
+                    sb.append("0").append("\n");
+                }
             }
-            if(count == N)   sb.append("0").append("\n");
         }
         System.out.print(sb);
     }
