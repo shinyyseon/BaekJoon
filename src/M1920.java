f29e1a4a74b4c8276063337046875421dfb06512
Yong Seon Shin <asdf4503@gwnu.ac.kr>
2023-03-21 15:15:05 +0900

    commit

--- src/M1920.java
+++ src/M1920.java
@@ -18,19 +18,7 @@ public class M1920 {
 
         for(int i = 0;i < N;i++) {
             int count = 0;
-            for(int j = 0;j < N;j++) {
-                if(array[i].equals(A[j])) {
-                    sb.append("1").append("\n");
-                    break;
-                }
-                else if(Integer.parseInt(A[j]) > Integer.parseInt(array[i])) {
-                    count++;
-                    break;
-                }
-                if(count == M) {
-                    sb.append("0").append("\n");
-                }
-            }
+
         }
         System.out.print(sb);
     }
