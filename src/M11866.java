eb932c9554218ba39d938a09572b7e270edbeca9
Yong Seon Shin <asdf4503@gwnu.ac.kr>
2023-04-03 17:37:02 +0900

    commit

--- src/M11866.java
+++ src/M11866.java
@@ -11,21 +11,42 @@ public class M11866 {
 
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        boolean[] array = new boolean[N];
-        int temp = 0;
+        int[] array = new int[N + 1];
+        int temp = 0, count = 0;
+
+        for(int i = 0;i < N;i++)    array[i] = i + 1;
 
         sb.append("<");
 
+        //ArrayList 사용해서 풀어볼 것
+        //해당 인덱스 삭제 (remove() 명령어 사용)
         while(true) {
-            temp += 3;
-            for(int i = 0;i < N;i++) {
-                if(temp > N) {
+            for(int i = 1;i <= K;i++) {
+                if(temp == N - 1)       temp = 0;
 
+                if(i == 3) {
+                    sb.append(array[temp]);
+                    array[temp] = 0;
+                    for(int j = 0;j < N;j++) {
+                        if(array[j] == 0) {
+                            array[j] = array[j + 1];
+                            array[j + 1] = 0;
+                            N -= 1;
+                        }
+                    }
+                    count++;
                 }
-                else {
-                    array[temp + 1] = false;
-                }
+                temp++;
             }
+            for(int i = 0;i < N;i++) {
+                System.out.println("\n" + array[i] + "\n");
+            }
+            if(count == N)  break;
+            else sb.append(", ");
         }
+
+        sb.append(">");
+
+        System.out.println(sb);
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -2,23 +2,50 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int list_size = 5; // 배열 사이즈
+        int[] list = new int[list_size]; // 배열의 크기가 5인 배열을 생성
+        int count = 0; // 배열 원소가 이동할 때 마다 count
+
+        for(int j = 0; j < 5; j++) {
+            list[j] = j+1; // 인덱스가 i일 때 배열 값은 i+1을 넣는다.
+        }
+
+        for(int i = 1; i < list.length-1; i++) { // 우리가 삭제하고 싶은 인덱스 값부터 마지막 인덱스까지 반복문을 돌린다.
+            list[i] = list[i+1];  // 인덱스 i+1의 값을 인덱스 i로 이동
+            count++;  // 이동할 때마다 count 증가
+        }
+
+        System.out.println(count); // counting을 확인한다.
+        System.out.println(list[1]); // 우리가 원하는 배열 값을 확인한다.
+
+        for(int i : list)   System.out.print(i + " ");
+
+       /* BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(bf.readLine());
-        String[] str = new String[n];
+        int[] array = new int[n + 1];
 
         for(int i = 0;i < n;i++) {
-            str[i] = bf.readLine();
-            System.out.println(Integer.parseInt(str[i]));
+            array[i] = i + 1;
         }
 
-        Arrays.sort(str);
+        array[3] = 0;
 
         for(int i = 0;i < n;i++) {
-            System.out.println(str[i]);
+            if(array[i] == 0) {
+                array[i] = array[i + 1];
+                array[i + 1] = 0;
+                //n -= 1;
+            }
         }
+
+        for(int i : array) {
+            System.out.print(i + " ");
+        }*/
     }
 }
