8424481ed6d167eda540ede9c6bb22368bbeb3a8
Yong Seon Shin <asdf4503@gwnu.ac.kr>
2023-05-04 10:49:08 +0900

    commit

--- src/M11866.java
+++ src/M11866.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class M11866 {
@@ -11,42 +12,32 @@ public class M11866 {
 
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int[] array = new int[N + 1];
-        int temp = 0, count = 0;
 
-        for(int i = 0;i < N;i++)    array[i] = i + 1;
+        ArrayList<Integer> array= new ArrayList<>(N);
+
+        for(int i = 0;i < N;i++)
+            array.add(i+1);
 
         sb.append("<");
+        int count = 0;
+        int index = 0;
 
-        //ArrayList 사용해서 풀어볼 것
-        //해당 인덱스 삭제 (remove() 명령어 사용)
         while(true) {
-            for(int i = 1;i <= K;i++) {
-                if(temp == N - 1)       temp = 0;
+            //index += K;
 
-                if(i == 3) {
-                    sb.append(array[temp]);
-                    array[temp] = 0;
-                    for(int j = 0;j < N;j++) {
-                        if(array[j] == 0) {
-                            array[j] = array[j + 1];
-                            array[j + 1] = 0;
-                            N -= 1;
-                        }
-                    }
-                    count++;
-                }
-                temp++;
-            }
-            for(int i = 0;i < N;i++) {
-                System.out.println("\n" + array[i] + "\n");
-            }
-            if(count == N)  break;
-            else sb.append(", ");
+            index = (index + (K - 1)) % array.size();
+
+            sb.append(array.get(index));
+            array.remove(index);
+            count++;
+
+            if(count == N)
+                break;
+            else
+                sb.append(", ");
         }
-
         sb.append(">");
 
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
