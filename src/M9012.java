1bcfc1caa4e36ff9cefccde25ee1ba71ad7270b5
asdf4503 <asdf4503@gwnu.ac.kr>
2023-10-18 23:44:07 +0900

    commit

--- src/M9012.java
+++ src/M9012.java
@@ -7,7 +7,6 @@ public class M9012 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-
         int n = Integer.parseInt(bf.readLine());
         for(int i = 0;i < n;i++) {
             String[] s = bf.readLine().split("");
